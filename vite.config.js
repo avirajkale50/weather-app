b5c0644e2267363c9c65f84134beed64d4b134e8
avirajkale50 <avirajkale2004@gmail.com>
2025-02-09 10:38:28 +0530

    pwa added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
 })
+
+
+
